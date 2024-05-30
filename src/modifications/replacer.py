1e2139313a5ab84279e11b8c703a90d518567ce1
Filip Ciąder <filip.ciader@gmail.com>
2024-05-30 16:32:26 +0200

    New version that checks if there are not linkage change already provided (to be tested).

--- src/modifications/replacer.py
+++ src/modifications/replacer.py
@@ -2,32 +2,42 @@ import re
 from typing import List
 
 class TextReplacer:
-    "A class to replace links to brackets."
+    """A class to replace links to brackets."""
 
     def __init__(self, given_word: str):
         self.given_word = given_word
 
     def replace_href(self, lines: List[str]) -> List[str]:
-        """Replaces all occurrences of href="address" with href="{% given_word 'address' %}"."""
-
+        """Replaces all occurrences of href="address" with href="{% given_word 'address' %}", skipping those already in the correct format."""
         pattern = r'href\s*=\s*"([^"]+)"'
         replacement = r'href = "{% ' + self.given_word + r" '\1' %}" + r'"'
-        new_lines = ["{"+"% "+ self.given_word +" %"+"}\n"]
-        new_lines+=[re.sub(pattern, replacement, line) for line in lines]
+        proper_format_pattern = r'href\s*=\s*"{%\s*' + self.given_word + r"\s*'[^']+'\s*%}"
+        new_lines = ["{" + "% " + self.given_word + " %}\n"]
+
+        for line in lines:
+            if re.search(proper_format_pattern, line):
+                new_lines.append(line)
+            else:
+                new_lines.append(re.sub(pattern, replacement, line))
 
         return new_lines
 
     def replace_src(self, lines: List[str]) -> List[str]:
-        """Replaces all occurrences of src="address" with src="{% given_word 'address' %}"."""
-
+        """Replaces all occurrences of src="address" with src="{% given_word 'address' %}", skipping those already in the correct format."""
         pattern = r'src\s*=\s*"([^"]+)"'
         replacement = r'src = "{% ' + self.given_word + r" '\1' %}" + r'"'
-        new_lines = ["{"+"% "+ self.given_word +" %"+"}\n"]
-        new_lines+=[re.sub(pattern, replacement, line) for line in lines]
+        proper_format_pattern = r'src\s*=\s*"{%\s*' + self.given_word + r"\s*'[^']+'\s*%}"
+        new_lines = ["{" + "% " + self.given_word + " %}\n"]
+
+        for line in lines:
+            if re.search(proper_format_pattern, line):
+                new_lines.append(line)
+            else:
+                new_lines.append(re.sub(pattern, replacement, line))
 
         return new_lines
 
     def replace(self, lines: List[str]) -> List[str]:
-        "replaces both href's and src's."
+        """Replaces both href's and src's, skipping those already in the correct format."""
         replaced_0 = self.replace_href(lines)
-        return       self.replace_src (replaced_0)[1:]
+        return self.replace_src(replaced_0)[1:]
